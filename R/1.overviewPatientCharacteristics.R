67aa1719cb447feff64ce852634c0ae31e0bf207
job <j.vanriet@erasmusmc.nl>
2022-04-01 13:24:00 +0200

    Sync

--- R/1.overviewPatientCharacteristics.R
+++ R/1.overviewPatientCharacteristics.R
@@ -26,6 +26,7 @@ data.Patient$Hematology <- readxl::read_excel('Misc./Suppl. Table 1 - Overview o
 data.Patient$PSA <- readxl::read_excel('Misc./Suppl. Table 1 - Overview of Data.xlsx', trim_ws = T, sheet = 'PSA Measurements', col_types = c('text', 'date', 'numeric'))
 data.Patient$priorChemo <- readxl::read_excel('Misc./Suppl. Table 1 - Overview of Data.xlsx', trim_ws = T, sheet = 'Prior Treatment - Chemo', col_types = c('text', 'text', 'date', 'date', 'numeric', 'text'))
 data.Patient$priorHormonal <- readxl::read_excel('Misc./Suppl. Table 1 - Overview of Data.xlsx', trim_ws = T, sheet = 'Prior Treatment - Hormonal', col_types = c('text', 'text', 'date', 'date', 'text'))
+data.Patient$AUC <- readxl::read_excel('Misc./Suppl. Table 1 - Overview of Data.xlsx', trim_ws = T, sheet = 'AUC', col_types = c('numeric', 'text'))
 
 
 # Table 1 - Patient Characteristics ----
@@ -35,11 +36,11 @@ data.Patient$clinicalData <- data.Patient$clinicalData %>%
     # Add AR-V7 status (on baseline).
     dplyr::left_join(data.Patient$Overview %>% dplyr::distinct(`Subject Number`, `Inclusion (Treated with Caba)`, `AR-V7 (Baseline)`)) %>%
     dplyr::mutate(`AR-V7 (Baseline)` = ifelse(`Inclusion (Treated with Caba)` == 'Yes' & `AR-V7 (Baseline)` == 'Pos.', 'Main - Pos.', `AR-V7 (Baseline)`)) %>%
-
+    
     dplyr::mutate(postTreatment_Clean = ifelse(grepl('Cabazitaxel|Enzalu|Abir', `Post-Treatment`), `Post-Treatment`, 'Other')) %>%
     dplyr::mutate(postTreatment_Clean = ifelse(`Post-Treatment` == 'Unknown', 'Unknown', postTreatment_Clean)) %>%
     dplyr::mutate(postTreatment_Clean = ifelse(grepl('Cabazitaxel', `Post-Treatment`), 'Cabazitaxel', postTreatment_Clean)) %>%
-
+    
     # Determine which patients received prior local therapy
     dplyr::group_by(`Subject Number`) %>%
     dplyr::mutate(
@@ -167,13 +168,13 @@ responseCTC <- data.Patient$Overview %>%
     dplyr::select(`Subject Number`, `CTC Count (Baseline – 7.5mL)`, `CTC – Treatment 3`, `CTC – Treatment 4`, `CTC – Treatment 5`) %>%
     dplyr::group_by(`Subject Number`) %>%
     dplyr::mutate(
-
+        
         # Check if any CTC-measurement is below five CTC after caba-treatment.
         ctcResponse = ifelse(min(`CTC – Treatment 3`, `CTC – Treatment 4`, `CTC – Treatment 5`, na.rm = T) < 5, 'Formal Response (# CTC after any caba-treatment < 5)', 'No Response'),
-
+        
         # Check if any CTC-measurement after caba-treatment is below 50% of the baseline CTC count.
         ctcDecline = ifelse(any((na.omit(c(`CTC – Treatment 3`, `CTC – Treatment 4`, `CTC – Treatment 5`)) / `CTC Count (Baseline – 7.5mL)`) <= .5), 'Formal Decline (50% drop of baseline CTC (7.5 mL) in any caba-treatment)', 'No Decline')
-
+        
     ) %>%
     dplyr::ungroup()
 
@@ -181,26 +182,26 @@ responseCTC <- data.Patient$Overview %>%
 
 # Convert PSA measurements.
 data.PSA <- data.Patient$PSA %>%
-
+    
     # Filter on caba-treated patients.
     dplyr::full_join(data.Patient$clinicalData, by = 'Subject Number') %>%
     dplyr::full_join(data.Patient$Overview, by = 'Subject Number') %>%
     dplyr::filter(`Inclusion (Treated with Caba).x` == 'Yes') %>%
-
+    
     # Convert dates.
     dplyr::mutate_at(dplyr::vars(dplyr::contains('Date:')), as.Date) %>%
-
+    
     # Center on start of caba. treatment.
     dplyr::mutate(
         dayPSA.Caba = `Date: PSA measurement` - `Date: Start of Post-Treatment`,
         dayDeath.Caba = `Date: Death` - `Date: Start of Post-Treatment`,
-
+        
         # Correct for actual end of study.
         `Date: End of cabazitaxel cycles` = `Date: End of cabazitaxel cycles` + 21,
-
+        
         duringCaba = ifelse(dayPSA.Caba > 0 & `Date: PSA measurement` <= `Date: End of cabazitaxel cycles`, 'Yes', 'No')
     ) %>%
-
+    
     # Center on PSA prior to caba.
     dplyr::group_by(`Subject Number`) %>%
     dplyr::mutate(
@@ -215,10 +216,10 @@ data.PSA <- data.Patient$PSA %>%
         maxDeltaPSA.Decrease = ifelse(all(duringCaba == 'No'), NA, maxPSA.Decrease - max(PSA.First))
     ) %>%
     dplyr::ungroup() %>%
-
+    
     # Threshold PSA increase.
     dplyr::mutate(PSA.Centered = ifelse(PSA.Centered > 1, 1, PSA.Centered)) %>%
-
+    
     # Sort patients on total length of caba-treatment.
     dplyr::mutate(`Subject Number` = factor(`Subject Number`, levels = unique(`Subject Number`[order(-dayEndOfCabaCycles)])))
 
@@ -230,34 +231,34 @@ data.PSA <- data.PSA %>%
 # PSA per caba. treatment (per patient, centered on start of caba.).
 plot.PSA <- data.PSA %>%
     ggplot2::ggplot(aes(x = dayPSA.Caba, y = PSA.Centered)) +
-
+    
     # Initial pre-caba PSA (baseline).
     ggplot2::geom_hline(yintercept = 0, color = '#F6A18C', alpha = .5, lty = 11, size = .25) +
-
+    
     # Up/low line of CABA-V7 cycles.
     ggplot2::geom_rect(aes(ymin = -1.5, ymax = 1.5, xmin = 0, xmax = dayEndOfCabaCycles), fill = NA, color = 'royalblue2', lty = '11', lwd = .05) +
-
+    
     # PSA measurements.
     ggplot2::geom_line(size = .4, lty = 11, color = 'grey50') +
     ggplot2::geom_point(size = .75, color = 'black') +
-
+    
     # Show which PSA-point used as the baseline.
     ggplot2::geom_point(data = data.PSA %>% dplyr::filter(`PSA [ug/L]` == PSA.First, dayPSA.Caba <= 0), size = .9, color = 'royalblue2') +
-
+    
     # Show which PSA-point used as the max. diff decrease PSA.
     ggplot2::geom_point(data = data.PSA %>% dplyr::filter(`PSA [ug/L]` == maxPSA.Decrease, duringCaba == 'Yes'), size = .9, color = '#E87E0D') +
-
+    
     # Show days of death after Caba treatment.
     # ggplot2::geom_point(data = data.PSA %>% dplyr::filter(!is.na(`Date: Death`)) %>% dplyr::distinct(`Subject Number`, dayDeath.Caba), aes(x = dayDeath.Caba, y = 0), color = 'black', shape='\u2020', size = 3) +
-
+    
     # Scales.
     ggplot2::scale_y_continuous(breaks = c(-1, 0, 1), labels = c('-100%', '0%', '≥100%'), expand = c(.1, .1)) +
     ggplot2::scale_color_discrete(guide = 'none') +
     scale_x_continuous(breaks = seq(-25, 301, 25), expand = c(0,0), limits = c(-30, 310)) +
-
+    
     # Labs.
     ggplot2::labs(x = 'Number of days on cabazitaxel treatment', y = 'ΔPSA (in %), µg/L<br><span style = "font-size:6pt">(Centered on PSA prior to Cabazitaxel treatment)</span>') +
-
+    
     # Facet + themes
     facet_grid(`Subject Number` ~ ., scales = 'free_x', switch = 'y') +
     theme_Job +
@@ -282,20 +283,20 @@ plot.deltaPSA <- data.PSA %>%
         `Subject Number` = factor(`Subject Number`, levels = rev(levels(data.PSA$`Subject Number`)))
     ) %>%
     reshape2::melt(id.vars = 'Subject Number') %>%
-
+    
     ggplot2::ggplot(aes(x = variable, y = `Subject Number`, label = value, fill = value,)) +
-
+    
     # Add heatmap.
     ggplot2::geom_tile(width = 2, height = 1, colour = 'white', fill = 'white', lwd = .25, na.rm = T) +
     ggplot2::geom_text(size = 2.5, hjust = 0) +
-
+    
     # Labs
     ggplot2::labs(x = NULL, y = NULL) +
-
+    
     # Scales.
     ggplot2::scale_x_discrete(expand = c(0,0)) +
     ggplot2::scale_y_discrete(expand = c(0,0)) +
-
+    
     theme_Job +
     theme(
         text = element_text(family = 'Helvetica'),
@@ -316,20 +317,20 @@ plot.Response <- data.PSA %>%
         `Subject Number` = factor(`Subject Number`, levels = rev(levels(data.PSA$`Subject Number`))),
         variable = factor(variable, levels = c('Response PSA', 'Response CTC', 'Response CTC-Decline'))
     ) %>%
-
+    
     ggplot2::ggplot(aes(x = variable, y = `Subject Number`, fill = value)) +
-
+    
     # Add heatmap.
     ggplot2::geom_tile(width = .75, height = .75, colour = 'grey25', lwd = .25, na.rm = T) +
-
+    
     # Labs
     ggplot2::labs(x = NULL, y = NULL) +
-
+    
     # Scales.
     ggplot2::scale_x_discrete(expand = c(0,0)) +
     ggplot2::scale_y_discrete(expand = c(0,0)) +
     ggplot2::scale_fill_manual(values = c('Formal Decline (50% drop of baseline CTC (7.5 mL) in any caba-treatment)' = '#659A37', 'Formal Response (# CTC after any caba-treatment < 5)' = '#659A37', 'Formal Response (Two consecutive PSA decreases ≤ 50% from baseline PSA)' = '#659A37', 'No Decline' = 'white', 'No Response' = 'white', '.' = 'grey75'), na.value = 'grey75', guide = guide_legend(title = 'Response Type', title.position = 'top', title.hjust = .5, nrow = 4)) +
-
+    
     theme_Job +
     ggplot2::theme(
         text = element_text(family = 'Helvetica'),
@@ -378,3 +379,34 @@ test <- data.frame(
 
 chisq.test(test, simulate.p.value = T)
 fisher.test(test, simulate.p.value = T, alternative = 'two.sided')
+
+# AUC - Cabazitaxel exposure ----
+
+dataAUC <- data.Patient$AUC %>%
+    dplyr::group_by(Type) %>% 
+    dplyr::mutate(
+        median = median(`Cabazitaxel AUC_0-24h (ng*h/ML) – Dose Corrected`),
+        group = sprintf('%s<br>(<i>n</i> = %s)', Type, dplyr::n())
+    ) %>% 
+    dplyr::ungroup()
+
+stat.test <- dataAUC %>% 
+    dplyr::mutate(g = group, value = `Cabazitaxel AUC_0-24h (ng*h/ML) – Dose Corrected`) %>%
+    rstatix::pairwise_wilcox_test(value ~ g, exact = T, p.adjust.method = 'none', detailed = T, paired = F) %>%
+    rstatix::adjust_pvalue(method = 'BH') %>%
+    rstatix::add_significance(cutpoints = c(0, 0.001, 0.01, 0.05, 1), symbols = c('***', '**', '*', 'ns'))
+
+dataAUC %>% 
+    
+    ggplot2::ggplot(aes(x = group, y = `Cabazitaxel AUC_0-24h (ng*h/ML) – Dose Corrected`, fill = group, label = median)) +
+    
+    # Add half-half plots with median labels.
+    gghalves::geom_half_boxplot(side = 'l', outlier.shape = NA, notch = T, show.legend = F) +
+    gghalves::geom_half_point_panel(side = 'r', shape = 21, size = 1.25, color = 'black', position = ggbeeswarm::position_quasirandom(width = .2)) +
+    ggplot2::stat_summary(fun = median, colour='black', geom='text', size = 3, show.legend = FALSE, vjust=-2, angle = 90, hjust = .5) +
+    
+    ggpubr::geom_bracket(aes(xmin = group1, xmax = group2, label = p.adj, fill = NULL, color = NULL, shape = NULL), data = stat.test, y.position = 450, step.increase = .02, tip.length = .01) +
+    ggplot2::scale_fill_manual(values = c('#648FFF', '#FE6100', '#4D4D4D'), guide = 'none') +
+    ggplot2::labs(x = NULL, y = 'Cabazitaxel AUC<sub>0-24h</sub> (ng*h/mL)<br>Dose Corrected') +
+    ggplot2::scale_y_continuous(limits = c(0, 500), breaks = seq(0, 500, 100), expand = c(0,0)) +
+    theme_Job
